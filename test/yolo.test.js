6285a75083a0f5a90228767e29c7a4cdbf5af0f1
sbhmajum369 <majum.subh@gmail.com>
2022-01-26 15:54:23 -0500

    YOLO_test_update

--- test/yolo.test.js
+++ test/yolo.test.js
@@ -4,28 +4,32 @@ const tfnode = require('@tensorflow/tfjs-node');
 const path = require('path');
 const fs = require('fs');
 const assert = require('assert');
+
 require('regenerator-runtime');
 
 const yolov5 =  require('../packages/YOLO');
 
+
 const loadImage = (filename) => {
 	let image0, image, imageBuffer;
 	
-	const defaultImageShape = [1, 640, 640, 3];
+	const defaultImageShape = [640, 640, 3];
 
 	try {
 		imageBuffer = fs.readFileSync(path.join(__dirname, `./test_imgs/${filename}`));  // Pic_8_640.png  Zidane_640.jpg
-		console.log("Image Loaded \n");
+		console.log(`${filename} Loaded \n`);
 	} catch (err) {
 		console.error(`${err.message} \n`);
 		return null;
 	};
 	image0 = tfnode.node.decodeImage(imageBuffer, 3);
+
+	const matchImgShape = image0.shape.every((val, ind) => val === defaultImageShape[ind]);
 	
-	if (image0.shape !== defaultImageShape) {
+	if (!matchImgShape) {
 		console.log('RESIZING... \n');
 		// Resize the image
-		image0 = tf.image.resizeBilinear(image0, size = [defaultImageShape[1], defaultImageShape[2]]);
+		image0 = tf.image.resizeBilinear(image0, size = [defaultImageShape[0], defaultImageShape[1]]);
 	}
 	
 	image = tf.cast(image0, 'float32');
@@ -40,47 +44,24 @@ const loadModel = async(model) => {
 	console.log(status, "\n");
 	// console.log(model.model);
 	return status;
-}
-
-const runInferenceCPU = async(model, image) => {
-	console.log("Running: ", model.details().name);
-
-	if (image == null) {
-		console.log("I am not seeing an image !!");
-		return [null, null];
-	}
-
-	if (model.model != '') {
-		start = tf.util.now();
-		const result = await model.predict(image);
-		const predictions = model.getDetections(result);
-		end = tf.util.now()
-
-		result.dispose;
-		return [predictions, (end-start)];
-	} else {
-		console.error("Model not loaded \n");
-		return [null, null];
-	};
-
 };
-
-
 
 const imageLoad_test = () => {
 	const targetShape = [1,640,640,3];
 	
-	// for (let i=2; i<10; i++) {}
-	let i=5;
-	// Reading Image
-	const imgFilename = `Pic_${i}.jpg`;		// `Pic_${i}_640.jpg` 
-	let image = loadImage(imgFilename);
+	// let i=5;
+	for (let i=1; i<=8; i++) {
 	
-	describe('Image loading Test', () => {
-		it('should return a shape [1,640,640,3]', () => {
-			assert.deepEqual(image.shape, targetShape);
-		})
-	});
+		// Reading Image
+		const imgFilename = `Pic_${i}_640.jpg`;		// `Pic_${i}_640.jpg` 
+		let image = loadImage(imgFilename);
+		
+		describe('Image loading Test', () => {
+			it('should return a shape [1,640,640,3]', () => {
+				assert.deepEqual(image.shape, targetShape);
+			})
+		});
+	};
 	
 };
 
@@ -97,17 +78,8 @@ const modelLoad_test = async(inpModel) => {
 			// Loading Model
 			const status = await loadModel(model);
 			assert.equal(status, targetStat);
-		})
+		});
 	});
-
-
-	// // Running Inference
-	// [predictions, time_taken] = await runInferenceCPU(model=model1);
-
-	// if (predictions.length > 0) {
-	// 	console.log(`Predictions for image ${imgFilename}:`, predictions);
-	// 	console.log(`Time: ${time_taken/1000} s \n`)
-	// }
 	
 	model.dispose;
 }
